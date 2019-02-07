b2164327bbd66b1c92b88901853604c6ecb965e3
PATHOM OAJHAN <pathom_oajhan@cmu.ac.th>
2019-02-07 10:50:19 +0700

    Show client IP/port, browser information, accept-charset and accept encoding, and hook in to clientinfo

---
+++ Assignments/HW1/DNWS/ClientInfoPlugin.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNWS
+{
+  class ClientInfoPlugin : IPlugin
+  {
+    //protected static Dictionary<String, int> statDictionary = null;
+    public ClientInfoPlugin()
+    {
+      
+    }
+
+    public void PreProcessing(HTTPRequest request)
+    {
+      throw new NotImplementedException();
+    }
+    public HTTPResponse GetResponse(HTTPRequest request)
+    {
+      HTTPResponse response = null;
+      StringBuilder sb = new StringBuilder();
+            
+        string[] IP= request.getPropertyByKey("RemoteEndPoint").Split(":"); // Split IP value and Port value
+          //  request.getPropertyByKey("RemoteEndPoint");
+      sb.Append("<html><body><p>Client IP: " +IP[0] + "</p>"); // Show Client IP
+      sb.Append("<p>Client Port: " + IP[1]+"</p>"); // Show Client Port
+      sb.Append("<p>Browser Information: " + request.getPropertyByKey("User-Agent") + "</p>"); // Show browser information
+      sb.Append("<p>Accept-Language: " + request.getPropertyByKey("Accept-Language") + "</p>"); // Show accept-charset
+      sb.Append("<p>Accept-Encoding: " + request.getPropertyByKey("Accept-Encoding") + "</p>"); // Show accept encoding
+      sb.Append("</body></html>");
+      response = new HTTPResponse(200);
+      response.body = Encoding.UTF8.GetBytes(sb.ToString());
+      return response;
+    }
+
+    public HTTPResponse PostProcessing(HTTPResponse response)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
