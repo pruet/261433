d27f8d3090285b30f4ede4fcdb5e0c1d33e881d8
DONYARIT MEEBUNGPHRAO <donyarit_m@cmu.ac.th>
2019-02-08 15:50:30 +0700

    Add clientinfo

---
+++ Assignments/HW1/DNWS/clientinfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNWS
+{
+    class clientinfo : IPlugin
+    {
+        public void PreProcessing(HTTPRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HTTPResponse GetResponse(HTTPRequest request)    //600611030 and 600611015 teach me and give me advice
+        {
+            HTTPResponse response = null;
+            StringBuilder sb = new StringBuilder();
+            string[] ipport = request.getPropertyByKey("RemoteEndPoint").Split(':');
+            sb.Append("<html><body>Client IP: " + ipport[0] + "</br></br>");
+            sb.Append("Client Port: " + ipport[1] + "</br></br>");
+            sb.Append("Browser Information: " + request.getPropertyByKey("User-Agent") + "</br></br>");
+            sb.Append("Accept-Language: " + request.getPropertyByKey("Accept-Language") + "</br></br>");
+            sb.Append("Accept-Encoding: " + request.getPropertyByKey("Accept-Encoding"));
+            sb.Append("</body></html>");
+            response = new HTTPResponse(200);
+            response.body = Encoding.UTF8.GetBytes(sb.ToString());
+            return response;
+        }
+        public HTTPResponse PostProcessing(HTTPResponse response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
