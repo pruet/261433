8f97253a228c6fc7ead1c7e93016817773b2f7ec
Jirapawee_R <papp.papp.gt@gmail.com>
2019-02-28 15:42:00 +0700

    implement thread in ClientPlugin.cs

--- Assignments/HW1/DNWS/ClientPlugin.cs
+++ Assignments/HW1/DNWS/ClientPlugin.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading; //to use thread
+using System.Diagnostics; //guided by 600611010
+
 
 namespace DNWS
 {
@@ -31,17 +34,26 @@ namespace DNWS
         {
             HTTPResponse response = null;
             StringBuilder sb = new StringBuilder();
+
             string[] cli_info = request.getPropertyByKey("RemoteEndPoint").Split(":");
             string data = request.Inf;
             string[] detail  = data.Split("User-Agent:");
             string[] Browser = detail[1].Split("Accept-Language:");
             string[] LangAcc = Browser[1].Split("Accept-Encoding:");
             string[] EncodeAcc = LangAcc[1].Split("Connection:");
+
             sb.Append("<html><body><h4>Client IP: " + cli_info[0] + "</h4>");
             sb.Append("<html><body><h4>Client Port: " + cli_info[1] + "</h4>");
             sb.Append("<html><body><h4>Browser Information: " + Browser[0] + "</h4>");
             sb.Append("<html><body><h4>Accept Language: " + LangAcc[0] + "</h4>");
             sb.Append("<html><body><h4>Accept Encoding: " + EncodeAcc[0] + "</h4>");
+
+            //HW2 guided by 600611001//
+            sb.Append("<div>threadID : " + Thread.CurrentThread.ManagedThreadId + "</div><br>"); //tell that there is only one thread for one connection
+            sb.Append("<div>threadState : " + Thread.CurrentThread.ThreadState + "</div><br>"); //print status of the trade
+            sb.Append("<div>Thread_IsAlive : " + Thread.CurrentThread.IsAlive + "</div><br>"); //tell that if thread is working
+            sb.Append("<div>Number_of_thread : " + Process.GetCurrentProcess().Threads.Count + "</div><br>"); //tell that how many thread there are
+
             foreach (KeyValuePair<String, int> entry in statDictionary)
             {
                 sb.Append(entry.Key + ": " + entry.Value.ToString() + "<br />");
