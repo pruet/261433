bd2e7beebdfb12648c5b84daf51b278646da2388
DONYARIT MEEBUNGPHRAO <donyarit_m@cmu.ac.th>
2019-02-28 15:04:23 +0700

    multi thread

--- Assignments/HW1/DNWS/clientinfo.cs
+++ Assignments/HW1/DNWS/clientinfo.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
 
 namespace DNWS
 {
-    class clientinfo : IPlugin
+    class ClientInfo : IPlugin 
     {
         public void PreProcessing(HTTPRequest request)
         {
             throw new NotImplementedException();
         }
 
-        public HTTPResponse GetResponse(HTTPRequest request)    //600611030 and 600611015 teach me and give me advice
+        public HTTPResponse GetResponse(HTTPRequest request)//600611030 and 600611015 teach me and give me advice
         {
             HTTPResponse response = null;
             StringBuilder sb = new StringBuilder();
@@ -20,8 +29,10 @@ namespace DNWS
             sb.Append("Client Port: " + ipport[1] + "</br></br>");
             sb.Append("Browser Information: " + request.getPropertyByKey("User-Agent") + "</br></br>");
             sb.Append("Accept-Language: " + request.getPropertyByKey("Accept-Language") + "</br></br>");
-            sb.Append("Accept-Encoding: " + request.getPropertyByKey("Accept-Encoding"));
+            sb.Append("Accept-Encoding: " + request.getPropertyByKey("Accept-Encoding") + "</br></br>");
             sb.Append("</body></html>");
+            sb.Append("Number of Thread: " + Process.GetCurrentProcess().Threads.Count + "</br></br>"); //ref from https://stackoverflow.com/questions/15381174/how-to-count-the-amount-of-concurrent-threads-in-net-application
+            //600611030 and 600611015 give me advices
             response = new HTTPResponse(200);
             response.body = Encoding.UTF8.GetBytes(sb.ToString());
             return response;
