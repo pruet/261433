98fb693a860af44e75b6a13266a087230104936c
JirapasChuatrakul <alfa3225@hotmail.co.th>
2019-03-13 23:35:45 +0700

    Fix some problem -add int to getavailablethread also min and max -ManageThreadID -> ManageThreadId

--- Assignments/HW1/DNWS/ClientInfo.cs
+++ Assignments/HW1/DNWS/ClientInfo.cs
@@ -47,10 +47,10 @@ namespace DNWS
         //sb.Append("<br><br>Current number of thread: " + Process.GetCurrentProcess().Threads.Count); //Show number of thread
         //Below these will be thread pool information showing which the code is guide by these reference
         //Ref. https://stackoverflow.com/questions/5236493/active-thread-number-in-thread-pool
-        ThreadPool.GetAvailableThreads(out available, out ioCompletion);
-        ThreadPool.GetMinThreads(out Minworker, out MinioCompletion);
-        ThreadPool.GetMaxThreads(out Maxworker, out MaxioCompletion);
-        sb.Append("<br><br>Thread ID: " + Thread.CurrentThread.ManagedThreadID);
+        ThreadPool.GetAvailableThreads(out int available, out int ioCompletion);
+        ThreadPool.GetMinThreads(out int Minworker, out int MinioCompletion);
+        ThreadPool.GetMaxThreads(out int Maxworker, out int MaxioCompletion);
+        sb.Append("<br><br>Thread ID: " + Thread.CurrentThread.ManagedThreadId);
         sb.Append("<br><br>Min Thread: " + Minworker);
         sb.Append("<br><br>Max Thread: " + Maxworker);
         sb.Append("<br><br>Available Thread: " + available);
