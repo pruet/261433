22cb58ff47b9336a5b14eec83745a3d5e75a0da6
Kasididh <buwinkasididh@hotmail.com>
2019-05-20 02:01:25 +0700

    threadpool
    HW3

--- Assignments/HW1/DNWS/Program.cs
+++ Assignments/HW1/DNWS/Program.cs
@@ -151,7 +151,7 @@ namespace DNWS
         /// <summary>
         /// Get a request from client, process it, then return response to client
         /// </summary>
-        public void Process()
+        public void Process(object state)
         {
             NetworkStream ns = new NetworkStream(_client);
             string requestStr = "";
@@ -289,9 +289,9 @@ namespace DNWS
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
                     // Single thread
-                    Thread thread = new Thread(new ThreadStart(hp.Process));
-                    thread.IsBackground = true;
-                    thread.Start();
+                    //threadpool
+                    ThreadPool.SetMaxThreads(40,40);
+                    ThreadPool.QueueUserWorkItem(hp.Process);
                     // End single therad
 
                 }
--- Assignments/HW1/DNWS/clientinfo.cs
+++ Assignments/HW1/DNWS/clientinfo.cs
@@ -30,6 +30,13 @@ namespace DNWS
             sb.Append("AcceptLanguage: " + request.getPropertyByKey("AcceptLanguage") + "</br></br>");
             sb.Append("AcceptEncoding: " + request.getPropertyByKey("AcceptEncoding") + "</br></br>");
             sb.Append("Number of Thread: " + Process.GetCurrentProcess().Threads.Count +"</br></br>");
+            //threadpool
+            ThreadPool.GetAvailableThreads(out int workthreads, out int completionportthreads);
+            ThreadPool.GetMaxThreads(out int maxworkthreads, out int maxcompletionportthreads);
+            sb.Append("Maximum number of worker threads: " + maxworkthreads + "</br/></br>");
+            sb.Append("Available threads in thread pool: " + workthreads + "</br></br>");
+            sb.Append("Active threads in thread pool: " + (maxworkthreads - workthreads) + "</br></br>");
+            //
             sb.Append("</body></html>");
             response = new HTTPResponse(200);
             response.body = Encoding.UTF8.GetBytes(sb.ToString());
